2b8f48793c06816cb7f825e470c882b605371887
Magda KO <79726830+malaneko@users.noreply.github.com>
2023-05-30 16:11:32 +0100

    commit  - questions
    index.js - questions created
    readme - questions section created

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # 11_node_readme_generator
 a command-line application that dynamically generates a professional README.md file from a user's input
+
+
+
+## Questions 
+* The title of my project 
+* Description 
+* Table of Contents 
+* Installation 
+* Usage 
+* License 
+* Contributing 
+* Tests 
+
+
+
+
+
+
+* uses ‘INQUIRER’   PACKAGE to  create command line PROMPTS 
+
+
+
+
+//-------------------------------------
--- index.js
+++ index.js
@@ -1,11 +1,41 @@
+
+// all dependencies:
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
-
+const questions = [{
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'readme'
+    },
+    {
+        type: 'input',
+        message: 'What is the description of your project?',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Enter installation information',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Enter usage information',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Enter contributing information',
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: 'Enter tests information',
+        name: 'tests'
+    },
 ];
 
 // function to write README file
